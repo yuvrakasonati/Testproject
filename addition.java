471d681e847c9c462f75f6481b635cba47d6e52f
yuvrakasonati <107368467+yuvrakasonati@users.noreply.github.com>
2022-06-13 02:03:52 +0530

    new code

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class additon
+{
+    public static void main(String[] args) {
+     Scanner x = new Scanner(System.in);
+     System.out.println("enter 2 numbers ");
+     int i = x.nextInt();
+     int j = x.nextInt();
+     int c = i+j;
+     System.out.println(c);
+    }
+}
+    
